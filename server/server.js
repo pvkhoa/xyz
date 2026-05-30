64e711f8e0876646b9c293d852db604fd93eac28
Phạm Việt Khoa <24100058@st.phenikaa-uni.edu.vn>
2026-05-30 07:26:34 +0000

    10đ

--- server/server.js
+++ server/server.js
@@ -1,8 +1,31 @@
-const express=require('express');
-const app=express();
-const port=9999;
+const express = require('express');
+const app = express();
+const port = 9999;
 
-app.listen(port, ()=>
-{
-    console.log("server is running on: ", port);
+app.listen(port, ()=> {
+    console.log("server is running on ", port);
+});
+app.get('/api/get', (res, req)=>{
+    req.json({
+        "message":"test successfully", 
+        "id":"md001"      
+    });
+});
+app.post('/api/post', (req, res)=>{
+    //req.body
+    body = req.body;
+    //req.headers
+    headers = req.headers;
+    headerID = req.headers['headerID'];
+    //req.query
+    param = req.query.paramID
+        
+    res.json({
+        'message':'post',
+        'body':body,
+        'headers': headers,
+        'headerID':headerID,
+        'param':param
+    })
+
 });
